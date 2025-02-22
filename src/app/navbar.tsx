7569ba634223950cf4d6a399bb01a2628950fd46
Nguyen An Phuc <nguyenvuidanang@gmail.com>
2025-02-22 08:57:11 +0700

    Responsive Navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,28 +1,44 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="navbar">
+    <nav className="navbar navbar-expand-md">
       <div className="container d-flex justify-content-between align-items-center">
-        {/* Logo (on the left) */}
+        {/* Logo */}
         <Link className="navbar-brand text-white" href="/">
           MyLogo
         </Link>
 
-        {/* Nav Links (on the right) */}
-        <div>
-          <Link className="nav-link d-inline px-3 text-white" href="/">
-            Home
-          </Link>
-          <Link className="nav-link d-inline px-3 text-white" href="/about">
-            About
-          </Link>
-          <Link className="nav-link d-inline px-3 text-white" href="/projects">
-            Projects
-          </Link>
-          <Link className="nav-link d-inline px-3 text-white" href="/contact">
-            Contact
-          </Link>
+        {/* Toggler Button */}
+        <button
+          className="navbar-toggler text-white border-0"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          ☰
+        </button>
+
+        {/* Nav Links (Collapsible) */}
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
+          <div className="navbar-nav ms-auto text-center">
+            <Link className="nav-link px-3 text-white fs-sm" href="/">
+              Home
+            </Link>
+            <Link className="nav-link px-3 text-white fs-sm" href="/about">
+              About
+            </Link>
+            <Link className="nav-link px-3 text-white fs-sm" href="/projects">
+              Projects
+            </Link>
+            <Link className="nav-link px-3 text-white fs-sm" href="/contact">
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
